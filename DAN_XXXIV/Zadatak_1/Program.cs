d25ddadaaf2a2fb42c2d4acca8b1f4197460d5f3
Milana8 <91amilana@gmail.com>
2020-06-18 11:24:24 +0200

    Metoda RaisingMoney

--- DAN_XXXIV/Zadatak_1/Program.cs
+++ DAN_XXXIV/Zadatak_1/Program.cs
@@ -8,6 +8,31 @@ namespace Zadatak_1
 {
     class Program
     {
+        public static object l = new object();
+        static int cash = 10000;
+
+        public static void RaisingMoney(int amount)
+        {
+
+
+            Random random = new Random();
+            amount = random.Next(100, 10001);
+            lock (l)
+            {
+                if (cash >= amount)
+                {
+                    Console.WriteLine(amount + "RSD were withdrawn from the ATM");
+                    cash = cash - amount;
+                    Console.WriteLine(cash + "RSD left at the ATM");
+
+                }
+                else
+                {
+                    Console.WriteLine("There is not enough money at the ATM");
+                }
+            }
+            Console.ReadLine();
+        }
         static void Main(string[] args)
         {
         }
