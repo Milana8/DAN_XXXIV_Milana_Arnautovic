3a217c39969e75a15ec1260cfeeae822fed4c75b
Milana8 <91amilana@gmail.com>
2020-06-18 12:33:28 +0200

    Naming, starting threads, adding comments in code

--- DAN_XXXIV/Zadatak_1/Program.cs
+++ DAN_XXXIV/Zadatak_1/Program.cs
@@ -11,28 +11,31 @@ namespace Zadatak_1
     {
         public static object l = new object();
         static int cash = 10000;
-
+        /// <summary>
+        /// ATM withdrawal method
+        /// </summary>
+        /// <param name="t"></param>
         public static void RaisingMoney(Thread t)
         {
 
 
-            lock (l)
+            lock (l) //locking one transaction
             {
                 Random random = new Random();
-                int amount = random.Next(100, 10001);
+                int amount = random.Next(100, 10001); //the amount that the user can withdraw is a random number between 100 and 10000
                 Thread.Sleep(15);
 
                 if (cash >= amount)
                 {
 
-                    Console.WriteLine(t.Name + " to withdraw " + amount + " RSD were withdrawn from the ATM");
+                    Console.WriteLine(t.Name + " to withdraw " + amount + " RSD");
                     cash = cash - amount;
-                    Console.WriteLine(cash + "RSD left at the ATM");
+                    Console.WriteLine(cash + "RSD left at the ATM\n");
 
                 }
                 else
                 {
-                    Console.WriteLine(t.Name + " The user tried to withdraw " + amount + " RSD but there is not enough money at the ATM");
+                    Console.WriteLine(t.Name + " The user tried to withdraw " + amount + " RSD but there is not enough money at the ATM\n");
                 }
             }
             Console.ReadLine();
@@ -46,7 +49,7 @@ namespace Zadatak_1
             Console.WriteLine("***WELCOME TO THE BANK***\n");
             Console.WriteLine("Enter the number of users of the first ATM:");
             bool e = int.TryParse(Console.ReadLine(), out numUsers1);
-            Console.WriteLine("Enter the number of users of the second ATM");
+            Console.WriteLine("Enter the number of users of the second ATM:");
             bool c = int.TryParse(Console.ReadLine(), out numUsers2);
             int user = numUsers1 + numUsers2;
 
@@ -55,10 +58,10 @@ namespace Zadatak_1
             for (int i = 0; i < numUsers1; i++)
             {
 
-                Thread t = new Thread(() => RaisingMoney(Thread.CurrentThread))
+                Thread t = new Thread(() => RaisingMoney(Thread.CurrentThread)) //creating threads
                 {
 
-                    Name = string.Format("User_{0} ATM1", i + 1)
+                    Name = string.Format("User_{0} from the ATM1", i + 1) //naming threads
 
                 };
 
@@ -70,10 +73,10 @@ namespace Zadatak_1
             for (int i= numUsers1; i < user; i++)
             {
 
-                Thread t = new Thread(() => RaisingMoney(Thread.CurrentThread))
+                Thread t = new Thread(() => RaisingMoney(Thread.CurrentThread)) //creating threads
                 {
 
-                    Name = string.Format("User_{0} ATM2", i + 1)
+                    Name = string.Format("User_{0} from the ATM2", i + 1) //naming threads
 
                 };
                 thr[i] = t;
@@ -82,7 +85,7 @@ namespace Zadatak_1
             }
             foreach (var item in thr)
             {
-                item.Start();
+                item.Start(); //starting threads
             }
             
         }
